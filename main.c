3d100e0d50d435487c2de1b3ca2e6ed4217c62a6
28add11 <kerbal2346@gmail.com>
2024-06-20 22:55:48 -0600

    Update to use minimum viable spi

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "hbi/hbi.h"
+#include "pico/time.h"
+#include "hardware/spi.h"
+//#include "hbi/hbi.h"
 #include <stdio.h>
 
-struct hbi mainLink;
+#define MASTER 1
 
 int main(void) {
     stdio_init_all();
@@ -16,18 +17,39 @@ int main(void) {
     sleep_ms(250);
     gpio_put(LED_PIN, 0);
 
-    start_hbi(pio0, 2, &mainLink); //start at pin 2 in case we want to debug with UART
-
 	printf("started\n");
 
-	while (1) {
-		uint32_t pingData = 1;
-		gpio_put(LED_PIN, 1);
-		printf("pingTime: %i\n", sendPing(&mainLink, pingData));
-		
-		sleep_ms(500);
-		gpio_put(LED_PIN, 0);
-		//tight_loop_contents();
+	spi_init(spi0, 1000000);
+
+	if (!MASTER) {
+		spi_set_slave(spi0, true);
 	}
+
+	gpio_set_function(2, GPIO_FUNC_SPI);
+	gpio_set_function(3, GPIO_FUNC_SPI);
+	gpio_set_function(4, GPIO_FUNC_SPI);
+	gpio_set_function(5, GPIO_FUNC_SPI);
+
+	if (MASTER) {
+	uint8_t pingDat = 0;
+	uint8_t returnDat;
+
+	while (1) {
+		uint32_t startTime = time_us_32();
+		spi_write_blocking(spi0, &pingDat, 1);
+		spi_read_blocking(spi0, 0, &returnDat, 1);
+		if (returnDat != pingDat) {
+			printf("Error: ping doesn't match");
+		}
+
+		uint32_t endTime = time_us_32() - startTime;
+		printf("%i\n", endTime);
+	}
+
+	} else {
+
+
+	}
+
     return 0;
 }
